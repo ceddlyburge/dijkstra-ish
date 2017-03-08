5c3be98e557757462f4711ced8741dc82664af68
CeddlyBurge <CeddlyBurge@gmail.com>
2017-03-08 17:47:01 +0000

    use %w in network topology tests

--- network_topology_tests.rb
+++ network_topology_tests.rb
@@ -3,37 +3,37 @@ require_relative 'leg'
 require_relative 'network_topology_parser.rb'
 
 class NetworkTopologyTests < Minitest::Test
-    def test_singe_leg
-        topology = NetworkTopologyParser.new ["AB5"]
+    def test_single_leg
+        topology = NetworkTopologyParser.new ['AB5']
 
-        assert_equal [route("A", "B", 5)], topology.original_legs
+        assert_equal [route('A', 'B', 5)], topology.legs
     end
 
     def test_two_legs
-        topology = NetworkTopologyParser.new ["AB5", "BC4"]
+        topology = NetworkTopologyParser.new %w(AB5 BC4)
 
-        assert_equal [route("A", "B", 5),
-                      route("B", "C", 4)], 
+        assert_equal [route('A', 'B', 5),
+                      route('B', 'C', 4)], 
                       topology.legs
     end
 
     def test_three_legs
-        topology = NetworkTopologyParser.new ["DE6", "AD5", "CE2"]
+        topology = NetworkTopologyParser.new %w(DE6 AD5 CE2)
 
-        assert_equal [route("D", "E", 6),
-                      route("A", "D", 5),
-                      route("C", "E", 2)], 
+        assert_equal [route('D', 'E', 6),
+                      route('A', 'D', 5),
+                      route('C', 'E', 2)], 
                       topology.legs
     end
 
     def test_invalid_input
-        topology = NetworkTopologyParser.new ["DE5", "AD5", "CE?"]
+        topology = NetworkTopologyParser.new %W(DE5 AD5 CE?)
         
         assert_equal [], topology.legs
-        refute_equal "", topology.error_message
+        refute_equal '', topology.error_message
     end
 
-    def route from, to, distance
+    def route(from, to, distance)
         return Leg.new from, to, distance
     end
 
