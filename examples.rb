dfc32bf431de1219cb54b189cc9132665e25801e
CeddlyBurge <CeddlyBurge@gmail.com>
2017-03-08 18:06:41 +0000

    Use single quotes instead of doubles

--- examples.rb
+++ examples.rb
@@ -20,15 +20,15 @@ class Examples
         puts %Q[Distance of route A-B-C: #{
             @route_permutations.distance(
                 desired_route(
-                    [route("A", "B"), 
-                     route("B", "C")]))
+                    [route('A', 'B'), 
+                     route('B', 'C')]))
             }]
     end
 
     def distance_a_d
         puts %Q[Distance of route A-D: #{
             @route_permutations.distance(
-                desired_route([route("A", "D")]))
+                desired_route([route('A', 'D')]))
             }]
     end
 
@@ -36,8 +36,8 @@ class Examples
         puts %Q[Distance of route A-D-C: #{
             @route_permutations.distance(
                 desired_route(
-                    [route("A", "D"), 
-                     route("D", "C")]))
+                    [route('A', 'D'), 
+                     route('D', 'C')]))
             }]
     end
 
@@ -45,25 +45,25 @@ class Examples
         puts %Q[Distance of route A-E-B-C-D: #{
             @route_permutations.distance(
                 desired_route(
-                    [route("A", "E"), 
-                     route("E", "B"), 
-                     route("B", "C"), 
-                     route("C", "D")]))
+                    [route('A', 'E'), 
+                     route('E', 'B'), 
+                     route('B', 'C'), 
+                     route('C', 'D')]))
             }]
     end
     
     def distance_a_e_d
         puts %Q[Distance of route A-E-D: #{
             @route_permutations.distance(
-                desired_route([route("A", "E"), route("E", "D")]))
+                desired_route([route('A', 'E'), route('E', 'D')]))
             }]
     end
 
     def trips_c_c_maximum_3_stops
         puts %Q[Number of trips starting at C and ending at C with a maximum of 3 stops: #{ \
             @route_permutations \
-            .non_retracing_permutations_from("C") \
-            .select{ | route_candidate | route_candidate.legs.last.to == "C" && route_candidate.legs.count <= 3}
+            .non_retracing_permutations_from('C') \
+            .select{ | route_candidate | route_candidate.legs.last.to == 'C' && route_candidate.legs.count <= 3}
             .count
         }]
     end
@@ -71,25 +71,25 @@ class Examples
     def trips_a_c_exactly_4_stops
         puts %Q[Number of trips starting at A and ending at C with exactly 4 stops: #{ \
             @route_permutations \
-            .all_permutations_capped_at_leg_count_from("A", 4) \
-            .select{ | route_candidate | route_candidate.legs.last.to == "C" && route_candidate.legs.count == 4}
+            .all_permutations_capped_at_leg_count_from('A', 4) \
+            .select{ | route_candidate | route_candidate.legs.last.to == 'C' && route_candidate.legs.count == 4}
             .count
         }]
     end
 
     def shortest_distance_a_c
-        puts %Q[Shortest distance A-C: #{@route_permutations.shortest_distance("A", "C")}]
+        puts %Q[Shortest distance A-C: #{@route_permutations.shortest_distance('A', 'C')}]
     end
 
     def shortest_distance_b_b
-        puts %Q[Shortest distance B-B: #{@route_permutations.shortest_distance("B", "B")}]
+        puts %Q[Shortest distance B-B: #{@route_permutations.shortest_distance('B', 'B')}]
     end
 
     def trips_c_c_distance_under_30
         puts %Q[Number of trips starting at C and ending at C with distance < 30: #{ \
             @route_permutations \
-            .all_permutations_capped_at_distance_from("C", 30) \
-            .select{ | route_candidate | route_candidate.legs.last.to == "C"}
+            .all_permutations_capped_at_distance_from('C', 30) \
+            .select{ | route_candidate | route_candidate.legs.last.to == 'C'}
             .count
         }]
     end
