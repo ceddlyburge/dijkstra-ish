3c7a50492d4791617ebec9df47ddf7fa1c0732f6
CeddlyBurge <CeddlyBurge@gmail.com>
2017-03-09 08:11:56 +0000

    Add route_extension_tests

---
+++ route_extension_tests.rb
@@ -0,0 +1,48 @@
+require 'simplecov'
+SimpleCov.start
+require 'minitest/autorun'
+require_relative 'leg'
+require_relative 'route_extension.rb'
+require_relative 'route_candidate.rb'
+
+class RouteCandidateTests < Minitest::Test
+
+  def test_retraces_existing_leg_true_if_extension_leg_in_original_legs
+    route_extension = RouteExtension.new [a_leg, a_different_leg], a_leg
+
+    assert_equal true, route_extension.retraces_existing_leg
+  end
+
+  def test_retraces_existing_leg_false_if_extension_leg_absent_from_original_legs
+    route_extension = RouteExtension.new [a_leg, a_leg], a_different_leg
+
+    assert_equal false, route_extension.retraces_existing_leg
+  end
+
+  def test_route_candidate_nil_if_extension_leg_nil
+    route_extension = RouteExtension.new [], nil
+
+    assert_nil route_extension.route_candidate
+  end
+
+  def test_route_candidate_contains_original_legs_and_extension_leg
+    route_extension = RouteExtension.new [a_leg], a_different_leg
+    route_candidate = RouteCandidate.new [a_leg, a_different_leg]
+
+    assert_equal route_candidate, route_extension.route_candidate
+  end
+
+  def a_leg
+    return Leg.new 'from', 'to', 1
+  end
+
+  def a_different_leg
+    return Leg.new 'from', 'not_to', 1
+  end
+
+  def setup
+    @unimportant = 0
+  end
+
+
+end
